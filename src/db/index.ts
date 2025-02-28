91d26f0b8ac28af76c61da0d5a69e28b75547925
xlehieu <xlehieu@gmail.com>
2025-02-28 16:50:51 +0700

    mongodb fix timeout

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,7 @@ const uri = process.env.DATABASE_URI;
 export async function connect() {
     try {
         await mongoose.connect(String(uri), {
+            connectTimeoutMS: 60000, //timeout khi kết nối
             serverSelectionTimeoutMS: 60000, // Tăng thời gian chờ chọn server lên 30 giây
             socketTimeoutMS: 60000, // Tăng thời gian chờ socket
         });
