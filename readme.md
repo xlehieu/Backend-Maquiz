6f8aad2753197fcec4f8ba666665dade3b09008c
xlehieu <xlehieu@gmail.com>
2025-02-28 13:14:12 +0700

    oi chan qua

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 // "start": "ts-node src/index.ts",
 // "dev": "nodemon -r tsconfig-paths/register src/index.ts",
 // "build": "tsc"
+{
+"version": 2,
+"rewrites": [{ "source": "/(.*)", "destination": "/api" }]
+}
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -6,9 +6,6 @@ import postRouter from './post.router';
 import * as UserService from '../app/services/user.service';
 import { Request, Response } from 'express';
 const routes = function (app: any) {
-    app.get('/api', (req: Request, res: Response) => {
-        res.json({ message: 'Hello nhe' });
-    });
     app.get('/api/hello-hieu-mao', async (req: Request, res: Response) => {
         const response = await UserService.getAllUser();
         res.json({ message: 'Successfully fetched', data: response });
@@ -17,5 +14,8 @@ const routes = function (app: any) {
     app.use('/api/quiz', quizRouter);
     app.use('/api/classroom', classroomRouter);
     app.use('/api/post', postRouter);
+    app.get('/api', (req: Request, res: Response) => {
+        res.json({ message: 'Hello nhe' });
+    });
 };
 export default routes;
