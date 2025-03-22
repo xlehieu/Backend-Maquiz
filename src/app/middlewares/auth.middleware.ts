8951c0ce82c34c4a714834a0ca3c88d4cec31c94
xlehieu <xlehieu@gmail.com>
2025-03-23 00:50:15 +0700

    vl

--- src/app/middlewares/auth.middleware.ts
+++ src/app/middlewares/auth.middleware.ts
@@ -48,10 +48,11 @@ export const authUserMiddleware = (req: any, res: Response, next: NextFunction):
     let token: any = req.cookies.access_token; //ở index.ts đã dùng app.use(cookieParser) nên ở d
     //if (!req.session.access_token) return res.status(401).json(); //.json({ status: 'ERR', message: 'Bạn cần đăng nhập' });
     token = token?.split(' ')[1];
+    console.log(token);
     //hàm verify này nhận dối số thứ 2 là khóa để giải mã
     // ở hàm general token bên jwtservice cũng là khóa process.env.access_token nên nó giải mã được
     if (!process.env.ACCESS_TOKEN) {
-        return res.status(500); //.json({ status: 'ERR', message: 'Lỗi' });
+        return res.sendStatus(500); //.json({ status: 'ERR', message: 'Lỗi' });
     }
     jwt.verify(token, process.env.ACCESS_TOKEN, function (err: any, user: any) {
         if (err) {
