33c6ee57f18066ba7d22ecd894d808204770ea86
xlehieu <xlehieu@gmail.com>
2025-03-22 22:29:34 +0700

    fix env

--- src/auth/googleAuth.ts
+++ src/auth/googleAuth.ts
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            callbackURL: 'http://localhost:3001/auth/google/callback',
+            callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3001/auth/google/callback',
         },
         (accessToken, refreshToken, profile, done) => {
             return done(null, profile);
