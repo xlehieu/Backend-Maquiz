89c2f3263b89e9e27ef2d3895ced86b652645af4
xlehieu <xlehieu@gmail.com>
2025-03-23 01:29:36 +0700

    oi vl lan cuoi nhe

--- src/app/controllers/auth/authGoogle.controller.ts
+++ src/app/controllers/auth/authGoogle.controller.ts
@@ -3,7 +3,13 @@ import * as AuthGoogleService from '../../services/auth/authGooogle.service';
 export const GoogleCallback = async (req: Request, res: Response): Promise<any> => {
     try {
         const response: any = await AuthGoogleService.GoogleCallback(req);
-        res.redirect(`${process.env.ALLOW_ORIGIN}/login-success?access_token=Bearer ${response.access_token}`);
+        res.cookie('access_token', `Bearer ${response.access_token}`, {
+            httpOnly: true, // Chặn truy cập từ JavaScript
+            secure: true, // Chỉ gửi qua HTTPS
+            sameSite: 'none', // Để cookie có thể gửi giữa các subdomain khác nhau
+            maxAge: 1000 * 60 * 60 * 24, // 1 ngày
+        });
+        res.redirect(`${process.env.ALLOW_ORIGIN}/login-success `);
     } catch (err: any) {
         return res.status(err.status || 500).json({});
     }
